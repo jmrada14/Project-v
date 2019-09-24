eca8007c4acd3d0e13d1dad8d83d5da4d5b37f2f
jmrada14 <jmrada14@tutanota.com>
2019-09-23 21:17:07 -0400

    socket.io added

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 let express = require("express");
 let exphbs = require("express-handlebars");
 let db = require("./models");
-
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 let app = express();
 let PORT = process.env.PORT || 3000;
 
@@ -32,9 +33,12 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(() => {
+  app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
