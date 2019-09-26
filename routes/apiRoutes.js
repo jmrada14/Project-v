0cc404b7ed18cb99d268fdae22a50abe72069361
jmrada14 <jmrada14@tutanota.com>
2019-09-26 13:20:03 -0400

    fixing travis

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,37 @@
-var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+let router = require('express').Router();
+let db = require('/models')
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+module.exports = router;
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+// GET /api/messages
+router.get('/', async (req, res, next) => {
+  try {
+    let messages = await db.Message.findAll();
+    res.json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /api/messages
+
+router.post('/', async (req, res, next) =>{
+
+  // We don't have proper users yet
+  try {
+    let user = await db.User.findOrCreate({
+      where: {
+        name: req.body.name || 'Juan'
+      }
     });
-  });
-};
+    let message = Message.build(req.body);
+    message.set(User, { save: false });
+    await message.save()
+    let returnMessage = message.toJSON();
+    returnMessage.user = user;
+    res.json(returnMessage);
+  } catch (err) {
+    next(err);
+  }
+});
--- socket/socketio.js
+++ socket/socketio.js
@@ -1,19 +1,20 @@
-let db = require('/models')
-
+let db = require("/models");
+console.log(db);
 module.exports = io => {
 
-    io.on('connection', socket => {
+    io.on("connection", socket => {
 
-        console.log(socket.id, ' is connected to the server!');
+        console.log(socket.id, "is connected to the server!");
 
-        socket.on('new-message', message => {
-            socket.broadcast.emit('new-message', message);
+        socket.on("new-message", message => {
+            socket.broadcast.emit("new-message", message);
         });
 
-        socket.on('new-room', channel => {
-            socket.broadcast.emit('new-room', channel);
+        socket.on("new-room", channel => {
+            socket.broadcast.emit("new-room", channel);
         });
 
     });
-return io
+return io;
+
 };
