dce78bb96d69d572f6ab7fb4464af461ceb609e0
lookwendyican <wendyalers@gmail.com>
2019-09-28 14:46:13 -0400

    linked css files to main handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,28 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/index", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index"
+      // {
+      //   msg: "Welcome!",
+      //   examples: dbExamples
+      // }
+      );
     });
-  });
+  // });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/login", function(req, res) {
+    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    res.render(
+      "login"
+      // , {
+      //   example: dbExample
+      //}
+    );
   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
