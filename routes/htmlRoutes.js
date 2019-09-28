f9ee2cb97a5f559006028f2f754c54a20ab445e3
jmrada14 <jmrada14@tutanota.com>
2019-09-28 14:36:51 -0400

    html api routes modified, rendering index

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,20 +3,8 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    db.Message.findAll({}).then(function(dbMessage) {
+      res.render("index");
     });
   });
 
