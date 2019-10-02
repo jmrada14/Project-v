78fbba37e24185d6ddb81e17d2a53675a4da91db
jmrada14 <jmrada14@tutanota.com>
2019-10-02 09:41:37 -0400

    fixing last merge, server working, database for users working, auth working.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
 // let db = require("../models");
 
-<<<<<<< HEAD
 // //get all messages
 // module.exports = app => {
 //   app.get("/api/messages", (req, res) => {
@@ -52,62 +51,3 @@
 //     });
 //   });
 // };
-=======
-//get all messages
-module.exports = (app) => {
-
-  app.get("/api/messages", (req, res) => {
-    let query = {};
-    if (req.query.user_id) {
-      query.UserId = req.query.user_id;
-    }
-    db.Message.findAll({
-      where: query,
-      include: [db.User]
-    }).then((dbMessage) => {
-      res.json(dbMessage);
-    });
-  });
-//get message by user
-  app.get("/api/messages/:id", (req, res) => {
-    db.Message.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.User]
-    }).then((dbMessage) => {
-      res.json(dbMessage);
-    });
-  });
-  //POST message
-  app.post("/api/messages", (req, res) => {
-    db.Message.create(req.body).then((dbMessage) => {
-      res.json(dbMessage);
-    });
-  });
- //GET ROOM
-  app.get("/api/room", (req, res) => {
-    let query = {};
-    // console.log(req.query)
-      query.UserId = 1;
-
-    db.Room.findAll({
-      // where: query,
-      include: [{
-        model: db.User,
-        through: {
-          where: query
-        }
-      }]
-    }).then((dbMessage) => {
-      res.json(dbMessage);
-    });
-  });
-// POST room
-app.post("/api/room", (req, res) => {
-  db.Room.create(req.body).then((dbRoom) => {res.json(dbRoom);
-  });
-});
-
-};
->>>>>>> master
