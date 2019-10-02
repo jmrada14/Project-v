521d8a2eaf7e3fc4a81fa330548cb0e95d3547f5
jmrada14 <jmrada14@tutanota.com>
2019-10-02 09:43:09 -0400

    travis fix

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     });
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-    User.prototype.validPassword =  (password) => {
+    User.prototype.validPassword = (password) => {
         return bcrypt.compareSync(password, this.password);
     };
     // Hooks are automatic methods that run during various phases of the User Model lifecycle
